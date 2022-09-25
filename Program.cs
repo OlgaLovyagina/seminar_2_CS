9aa81106b7d0a540e35df4fb5f15410f5d3733e3
Olga <ol_0524@mail.ru>
2022-09-26 01:31:50 +0300

    добавлено часть семинара 3 и дз

--- Program.cs
+++ Program.cs
@@ -116,3 +116,199 @@ else
 //    Console.WriteLine("Введите первое число : ");
 //    return Convert.ToInt32(Console.ReadLine());
 //}
+
+
+///Семинар 3
+///
+
+
+
+
+
+#region Task17
+/// <summary>
+/// Напишите программу, которая принимает на вход
+///координаты точки(X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+///номер четверти плоскости, в которой находится эта точка
+/// </summary>
+
+void Task17()
+{
+ // А здесь только ввод данных и вызов метода, ищет где лежит точка
+
+    Random random = new Random();
+    int x = random.Next(-10, 10);
+    int y = random.Next(-10, 10);
+    Console.WriteLine($"A({ x},{ y})");
+    CoordCheck(x, y);
+}
+
+void CoordCheck(int x, int y)
+    // Метод, который проверяет, где лежит точка
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Точка лежит в 1-ой четверти");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Точка лежит во 2-ой четверти ");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Точка лежит в 3-ей четверти ");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("Точка лежит в 4-ой четверти ");
+    }
+    else
+        Console.WriteLine("Точка лежит на одной из осей ");
+}
+#endregion
+
+#region Task18
+/// <summary>
+/// Напишите программу, которая по заданному номеру четверти, 
+/// показывает диапазон возможных координат точек в этой четверти (x и y).
+/// </summary>
+
+void Task18()
+{
+    Console.WriteLine("Введите номер четверти");
+    int number = Convert.ToInt32(Console.ReadLine());
+    CheckNam(number);
+}
+
+void CheckNam(int number)
+{
+    if (number == 1)
+    {
+        Console.WriteLine("x > 0, y > 0");
+    }
+    if (number == 2)
+    {
+        Console.WriteLine("x < 0, y > 0");
+    }
+    if (number == 3)
+    {
+        Console.WriteLine("x < 0, y < 0");
+    }
+    if (number == 4)
+    {
+        Console.WriteLine("x > 0, y < 0");
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не номер четверти");
+    }
+}
+#endregion
+
+#region Task21
+/// <summary>
+///Напишите программу, которая принимает на вход координаты двух точек и
+///находит расстояние между ними в 2D пространстве.
+/// </summary>
+/// 
+
+void Task21()
+{
+    Console.WriteLine("Введите координаты точки А: ");
+
+    Console.Write("X: ");
+    int ax = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Y: ");
+    int ay = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите координаты точки B: ");
+    Console.Write("X: ");
+    int bx = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Y: ");
+    int by = Convert.ToInt32(Console.ReadLine());
+
+    int k = bx - ax;
+    int m = by - ay;
+
+    double length = Math.Sqrt(k * k + m * m);
+    length = Math.Round(length, 2);
+    Console.WriteLine($"Длинна {length}");
+}
+
+#endregion
+
+#region Task21_1
+/// <summary>
+///Напишите программу, которая принимает на вход координаты двух точек и
+///находит расстояние между ними в 3D пространстве.
+/// </summary>
+/// 
+
+void Task21_1()
+{ 
+    Console.WriteLine("Введите координаты точки А: ");
+
+    Console.Write("X: ");
+    int ax = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Y: ");
+    int ay = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Z: ");
+    int az = Convert.ToInt32(Console.ReadLine());
+
+
+    Console.WriteLine("Введите координаты точки B: ");
+
+    Console.Write("X: ");
+    int bx = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Y: ");
+    int by = Convert.ToInt32(Console.ReadLine());    
+    Console.Write("Z: ");
+    int bz = Convert.ToInt32(Console.ReadLine());
+        
+
+    double length = Math.Sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay)
+        + (bz - az) * (bz - az));
+    length = Math.Round(length, 2);
+    Console.WriteLine($"Расстояние между точками {length}");
+}
+#endregion
+
+#region Task19
+/// <summary>
+///Напишите программу, которая принимает на вход пятизначное число и проверяет,
+///является ли оно палиндромом.
+/// </summary>
+
+
+void Task19()
+{
+    Console.WriteLine("Введите пятизначное целое число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int num = n;
+    int rev = 0;
+
+    while (n > 0)
+    {
+        rev = rev * 10 + n % 10;
+        n = n / 10;
+    }
+        if (num == rev)
+        {
+            Console.WriteLine("Введеное число является полиндромом ");
+        }
+        else
+        {
+            Console.WriteLine("Введеное число не является полиндромом ");
+        }
+}
+#endregion
+
+
+//Task17();
+//Task18();
+//Task21();
+//Task21_1();
+Task19();
+
+
+Console.ReadLine();
